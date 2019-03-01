f7f4e4b689d7c9fb29af1549fa435c44e3f9e633
Joe Antongiovanni <janton153@gmail.com>
2019-03-01 14:46:14 -0800

    recycler adapter framework

--- app/src/main/java/com/example/yourcoastandroid/AccessPointData/CCCAccPtDataStructure.java
+++ app/src/main/java/com/example/yourcoastandroid/AccessPointData/CCCAccPtDataStructure.java
@@ -2,7 +2,15 @@ package com.example.yourcoastandroid.AccessPointData;
 
 public class CCCAccPtDataStructure {
 
+    //variables for testing purposes with recycler view
+    private int ID;
+    private String DISTRICT;
 
+    public int getID() {
+        return ID;
+    }
 
-
+    public String getDISTRICT() {
+        return DISTRICT;
+    }
 }
--- app/src/main/java/com/example/yourcoastandroid/AccessPointData/CCCDataClient.java
+++ app/src/main/java/com/example/yourcoastandroid/AccessPointData/CCCDataClient.java
@@ -1,4 +1,50 @@
 package com.example.yourcoastandroid.AccessPointData;
 
-public class CCCDataClient {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.TextView;
+
+import com.example.yourcoastandroid.R;
+
+import java.util.ArrayList;
+
+//recycler adapter (may need to change name not sure if this is the correct file to implement this)
+public class CCCDataClient extends RecyclerView.Adapter<CCCDataClient.myViewHolder> {
+
+    private ArrayList<CCCAccPtDataStructure> list = new ArrayList<>();
+
+    CCCDataClient(ArrayList<CCCAccPtDataStructure> list){
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_view,viewGroup,false);
+        return new myViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myViewHolder viewHolder, int i) {
+        viewHolder.ID.setText(Integer.toString(list.get(i).getID()));
+        viewHolder.DISTRICT.setText(list.get(i).getDISTRICT());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;//list.size();
+    }
+
+    public static class myViewHolder extends RecyclerView.ViewHolder {
+        TextView ID, DISTRICT;
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ID = (TextView)itemView.findViewById(R.id.id);
+            DISTRICT = (TextView)itemView.findViewById(R.id.district);
+        }
+    }
 }
