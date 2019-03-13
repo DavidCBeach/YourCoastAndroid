789f539c1ced29363e149694b06b14443b6613ec
Ariel-McCarthy <somccarthy@csumb.edu>
2019-03-12 19:21:05 -0700

    added info button onto MapsActivity and created InfoActivity

---
+++ app/src/main/java/com/example/yourcoastandroid/InfoActivity.java
@@ -0,0 +1,40 @@
+package com.example.yourcoastandroid;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Add items to action bar
+        getMenuInflater().inflate(R.menu.menu_info, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Is item is selected, if so do correlated action
+        switch(item.getItemId())
+        {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
