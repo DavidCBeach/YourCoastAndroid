fca2f18ef3d9fe233dfc13da1112698a9dac830c
Ariel-McCarthy <somccarthy@csumb.edu>
2019-03-22 18:01:13 -0700

    Adding About page with strings.xml text

--- app/src/main/java/com/example/yourcoastandroid/InfoActivity.java
+++ app/src/main/java/com/example/yourcoastandroid/InfoActivity.java
@@ -3,8 +3,14 @@ package com.example.yourcoastandroid;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -12,6 +18,7 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        centerTitle();
     }
 
     @Override
@@ -36,5 +43,39 @@ public class InfoActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
 
+    }
+    private void centerTitle()
+    {
+        ArrayList<View> textViews = new ArrayList<>();
+
+        getWindow().getDecorView().findViewsWithText(textViews, getTitle(), View.FIND_VIEWS_WITH_TEXT);
+
+        if(textViews.size() > 0)
+        {
+            AppCompatTextView appCompatTextView = null;
+            if(textViews.size() == 1)
+            {
+                appCompatTextView = (AppCompatTextView) textViews.get(0);
+            }
+            else
+            {
+                for(View v : textViews)
+                {
+                    if(v.getParent() instanceof Toolbar)
+                    {
+                        appCompatTextView = (AppCompatTextView) v;
+                        break;
+                    }
+                }
+            }
+
+            if(appCompatTextView != null)
+            {
+                ViewGroup.LayoutParams params = appCompatTextView.getLayoutParams();
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                appCompatTextView.setLayoutParams(params);
+                appCompatTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            }
+        }
     }
 }
