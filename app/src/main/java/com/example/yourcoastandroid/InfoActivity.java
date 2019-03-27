2d7c8efabd056b27f8efd4fa38d9af5a8effd187
Ariel-McCarthy <somccarthy@csumb.edu>
2019-03-27 13:46:40 -0700

    Updated info page

--- app/src/main/java/com/example/yourcoastandroid/InfoActivity.java
+++ app/src/main/java/com/example/yourcoastandroid/InfoActivity.java
@@ -3,15 +3,36 @@ package com.example.yourcoastandroid;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import java.util.ArrayList;
 
 public class InfoActivity extends AppCompatActivity {
+
+    private WebView webViewInfoPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        centerTitle();
+
+        webViewInfoPage = findViewById(R.id.webViewInfoPage);
+
+        loadHTMLPage();
+    }
+
+    private void loadHTMLPage()
+    {
+        webViewInfoPage.getSettings().setJavaScriptEnabled(true);
+        webViewInfoPage.loadUrl("file:///android_asset/index.html");
+
     }
 
     @Override
@@ -36,5 +57,39 @@ public class InfoActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
 
+    }
+    private void centerTitle()
+    {
+        ArrayList<View> textViews = new ArrayList<>();
+
+        getWindow().getDecorView().findViewsWithText(textViews, getTitle(), View.FIND_VIEWS_WITH_TEXT);
+
+        if(textViews.size() > 0)
+        {
+            AppCompatTextView appCompatTextView = null;
+            if(textViews.size() == 1)
+            {
+                appCompatTextView = (AppCompatTextView) textViews.get(0);
+            }
+            else
+            {
+                for(View v : textViews)
+                {
+                    if(v.getParent() instanceof Toolbar)
+                    {
+                        appCompatTextView = (AppCompatTextView) v;
+                        break;
+                    }
+                }
+            }
+
+            if(appCompatTextView != null)
+            {
+                ViewGroup.LayoutParams params = appCompatTextView.getLayoutParams();
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                appCompatTextView.setLayoutParams(params);
+                appCompatTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            }
+        }
     }
 }
