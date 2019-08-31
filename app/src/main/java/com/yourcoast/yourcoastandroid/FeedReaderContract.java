93576982751467bcf49c0b08712a175b3a393e40
DavidCBeach <dbeach@csumb.edu>
2019-08-30 22:45:20 -0700

    adding sql base

--- app/src/main/java/com/yourcoast/yourcoastandroid/FeedReaderContract.java
+++ app/src/main/java/com/yourcoast/yourcoastandroid/FeedReaderContract.java
@@ -12,10 +12,56 @@ public final class FeedReaderContract {
     /* Inner class that defines the table contents */
     public static class FeedEntry implements BaseColumns {
         public static final String TABLE_NAME = "tasks";
-        public static final String COLUMN_NAME_CONTENT = "content";
-        public static final String COLUMN_NAME_DATE = "date";
-        public static final String COLUMN_NAME_STATUS = "status";
-        public static final String COLUMN_NAME_DATE_MILLI = "milli";
+        public static final String COLUMN_NAME_ID = "ID";
+        public static final String COLUMN_NAME_DISTRICT = "DISTRICT";
+        public static final String COLUMN_NAME_CountyNum = "CountyNum";
+        public static final String COLUMN_NAME_COUNTY = "NameMobileWeb";
+        public static final String COLUMN_NAME_DescriptionMobileWeb = "DescriptionMobileWeb";
+        public static final String COLUMN_NAME_PHONE_NMBR = "PHONE_NMBR";
+        public static final String COLUMN_NAME_FEE = "FEE";
+        public static final String COLUMN_NAME_PARKING = "PARKING";
+        public static final String COLUMN_NAME_DSABLDACSS = "DSABLDACSS";
+        public static final String COLUMN_NAME_RESTROOMS = "RESTROOMS";
+        public static final String COLUMN_NAME_VISTOR_CTR = "VISTOR_CTR";
+        public static final String COLUMN_NAME_DOG_FRIENDLY = "DOG_FRIENDLY";
+        public static final String COLUMN_NAME_EZ4STROLLERS = "EZ4STROLLERS";
+        public static final String COLUMN_NAME_PCNC_AREA = "PCNC_AREA";
+        public static final String COLUMN_NAME_CAMPGROUND = "CAMPGROUND";
+        public static final String COLUMN_NAME_SNDY_BEACH = "SNDY_BEACH";
+        public static final String COLUMN_NAME_DUNES = "DUNES";
+        public static final String COLUMN_NAME_RKY_SHORE = "RKY_SHORE";
+        public static final String COLUMN_NAME_BLUFF = "BLUFF";
+        public static final String COLUMN_NAME_STRS_BEACH = "STRS_BEACH";
+        public static final String COLUMN_NAME_PTH_BEACH = "PTH_BEACH";
+        public static final String COLUMN_NAME_BLFTP_TRLS = "BLFTP_TRLS";
+        public static final String COLUMN_NAME_BLFTP_PRK = "BLFTP_PRK";
+        public static final String COLUMN_NAME_WLDLFE_VWG = "WLDLFE_VWG";
+        public static final String COLUMN_NAME_TIDEPOOL = "TIDEPOOL";
+        public static final String COLUMN_NAME_VOLLEYBALL = "VOLLEYBALL";
+        public static final String COLUMN_NAME_FISHING = "FISHING";
+        public static final String COLUMN_NAME_BOATING = "BOATING";
+        public static final String COLUMN_NAME_LIST_ORDER = "LIST_ORDER";
+        public static final String COLUMN_NAME_GEOGR_AREA = "GEOGR_AREA";
+        public static final String COLUMN_NAME_LATITUDE = "LATITUDE";
+        public static final String COLUMN_NAME_LONGITUDE = "LONGITUDE";
+        public static final String COLUMN_NAME_Photo_1 = "Photo_1";
+        public static final String COLUMN_NAME_Photo_2 = "Photo_2";
+        public static final String COLUMN_NAME_Photo_3 = "Photo_3";
+        public static final String COLUMN_NAME_Photo_4 = "Photo_4";
+        public static final String COLUMN_NAME_Bch_whlchr = "Bch_whlchr";
+        public static final String COLUMN_NAME_BIKE_PATH = "BIKE_PATH";
+        public static final String COLUMN_NAME_BT_FACIL_TYPE = "BT_FACIL_TYPE";
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
--- app/src/main/java/com/yourcoast/yourcoastandroid/FeedReaderDbHelper.java
+++ app/src/main/java/com/yourcoast/yourcoastandroid/FeedReaderDbHelper.java
@@ -8,10 +8,44 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + FeedReaderContract.FeedEntry.TABLE_NAME + " (" +
                     FeedReaderContract.FeedEntry._ID + " INTEGER PRIMARY KEY," +
-                    FeedReaderContract.FeedEntry.COLUMN_NAME_CONTENT + " TEXT," +
-                    FeedReaderContract.FeedEntry.COLUMN_NAME_STATUS + " INT," +
-                    FeedReaderContract.FeedEntry.COLUMN_NAME_DATE_MILLI + " NUMERIC," +
-                    FeedReaderContract.FeedEntry.COLUMN_NAME_DATE + " TEXT)";
+    FeedReaderContract.FeedEntry.COLUMN_NAME_DISTRICT + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_CountyNum + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_COUNTY + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_DescriptionMobileWeb + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_PHONE_NMBR + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_FEE + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_PARKING + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_DSABLDACSS + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_RESTROOMS + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_VISTOR_CTR + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_DOG_FRIENDLY + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_EZ4STROLLERS + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_PCNC_AREA + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_CAMPGROUND + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_SNDY_BEACH + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_DUNES + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_RKY_SHORE + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_BLUFF + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_STRS_BEACH + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_PTH_BEACH + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_BLFTP_TRLS + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_BLFTP_PRK + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_WLDLFE_VWG + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_TIDEPOOL + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_VOLLEYBALL + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_FISHING + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_BOATING + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_LIST_ORDER + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_GEOGR_AREA + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_LATITUDE + " INTEGER," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_LONGITUDE + " INTEGER," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_Photo_1 + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_Photo_2 + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_Photo_3 + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_Photo_4 + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_Bch_whlchr + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_BIKE_PATH + " TEXT," +
+    FeedReaderContract.FeedEntry.COLUMN_NAME_BT_FACIL_TYPE + " TEXT)" ;
 
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + FeedReaderContract.FeedEntry.TABLE_NAME;
